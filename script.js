212f96f1a29e5510b2a6f4e9aef2a82da0a3e1d7
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 09:51:41 +0200

    added hw 3

---
+++ script.js
@@ -0,0 +1,62 @@
+// 1 завдання
+
+// let a = +prompt("Введіть натуральне число");
+// let result = 1;
+// for(let i = 1; i<=a;i++){
+//     result=result*i;
+// }
+// console.log(result);
+
+// 2 завдання
+
+// for(let count = 1000; count<=9999;count = count + 3){
+//    console.log(count);
+// }
+
+// 3 завдання
+
+// for(let count = 1; count<=55;count++){
+//    console.log(count);
+// }
+
+// 4 завдання
+
+// for(let count = 90; count>=0;count=count-5){
+//    console.log(count);
+// }
+
+// 5 завдання
+
+// let result = 1;
+// for(let i = 1;i<=20;i++){
+//    result = result*2;
+//    console.log(result);
+// }
+
+// 6 завдання
+
+// for(let a = 2;a<=10000;a = 2*a-1){
+//    console.log(a);
+// }
+
+// 7 завдання
+
+// let a = -166;
+// for(a;a<=99;a = 2*a+200){
+//    console.log(a);
+// }
+
+// 8 завдання
+
+let a = +prompt ("Введіть число:");
+let b = +prompt("Введіть степінь: ");
+let result = 1;
+if(a>0,b>0){
+   for(let i = 1; i<=b; i++){
+      result = result * a;
+  }
+  console.log(result);
+}
+else if(a>0,b<0){
+   console.log(1/result);
+}
